17a4275f2d60b6c1f94686c7b46aca8e7eeaebdd
ThatoSeluku <Thato1000Seluku@gmail.com>
2025-02-28 13:48:14 +0200

    Snake initialised to 3 squares and moves from 0, 0 coordinates in a downward direction

--- Snake.py
+++ Snake.py
@@ -1,45 +1,71 @@
 from tkinter import * 
 import random
-#Game constants 
+
+# Game constants 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3 
-SNAKE_COLOR= "#00FF00"
+SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
-
-
-
 
 class Snake:
         def __init__(self):
                 self.body_size = BODY_PARTS
-                self.coodinates = []
+                self.coordinates = []  # Fixed typo here
                 self.squares = []
 
                 for i in range(0, BODY_PARTS):
-                        self.coodinates.append([0,0])
+                        self.coordinates.append([0, 0])  # Fixed typo here
 
-                for x, y in self.coodinates:
-                        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill= SNAKE_COLOR, tag= "snake")
+                for x, y in self.coordinates:  # Fixed typo here
+                        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
                         self.squares.append(square)
-                        
 
 class Food:
         def __init__(self):
-              
                 x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
-                y  = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
-                
+                y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
                 self.coordinates = [x, y]
 
                 canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
-#Creation of game functions 
-def next_turn():
-        pass
+# Creation of game functions 
+def next_turn(snake, food):
+        # Initialise snake head coordinates
+        x, y = snake.coordinates[0]  # Fixed typo here
+
+        # Add snake movement logic:
+        # Up and down only relate to y coordinate movement:
+        if direction == "up":
+                y -= SPACE_SIZE
+        elif direction == "down":
+                y += SPACE_SIZE
+        # Left and right only relate to x coordinate movement:
+        elif direction == "left":
+                x -= SPACE_SIZE
+        elif direction == "right":
+                x += SPACE_SIZE
+
+
+
+
+        # Update snake head coordinates after click:
+        # 0 is the index x, and y are updated values selected by player
+        snake.coordinates.insert(0, (x, y))
+        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+        snake.squares.insert(0, square)
+
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+
+        del snake.squares[-1]
+
+        # Call next turn recursively
+        window.after(SPEED, next_turn, snake, food)
 
 def change_direction():
         pass
@@ -54,14 +80,13 @@ window = Tk()
 window.title("Snake 2025")
 window.resizable(False, False)
 
-
-score = 0;
+score = 0
 direction = 'down'
 
-label = Label(window, text = "Score: {}".format(score), font=('consolas', 40 ))
+label = Label(window, text="Score: {}".format(score), font=('consolas', 40))
 label.pack()
 
-canvas = Canvas(window, bg= BACKGROUND_COLOR, height= GAME_HEIGHT, width=GAME_WIDTH)
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
 window.update()
@@ -71,17 +96,18 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-#x and y variables casted as they cannot be floats:
-#Window is set to be in the center of your screen
-x = int((screen_width/2) - (window_width/2))
-y = int((screen_height/2) - (window_height/2))
+# x and y variables casted as they cannot be floats:
+# Window is set to be in the center of your screen
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
 
-#Set geometry:
+# Set geometry:
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-#Create snake and food objects:
+# Create snake and food objects:
 snake = Snake()
 food = Food()
 
+next_turn(snake, food)
 
 window.mainloop()
