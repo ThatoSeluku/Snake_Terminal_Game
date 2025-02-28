db492c2d018301eba77e28f87c6f11459a8d2640
ThatoSeluku <Thato1000Seluku@gmail.com>
2025-02-28 12:43:56 +0200

    Canvas added and score counter updated

--- Snake.py
+++ Snake.py
@@ -37,4 +37,14 @@ window.title("Snake 2025")
 window.resizable(False, False)
 
 
+score = 0;
+direction = 'down'
+
+label = Label(window, text = "Score: {}".format(score), font=('consolas', 40 ))
+label.pack()
+
+canvas = Canvas(window, bg= BACKGROUND_COLOR, height= GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+
 window.mainloop()
